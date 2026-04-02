e21840b4345d5d9845105a26e7db57094c5112f8
nibodika <nibodikachaudhary123@gmail.com>
2026-04-02 17:40:37 +0545

    Fixed echo command

--- script1.sh
+++ script1.sh
@@ -4,4 +4,4 @@
  sudo apt install apache2 -y
  sudo systemctl start apache2
 
- echo "Hello Nibodika, you are doing well!">/var/www/html/index.html
+ echo "Hello Nibodika, you are doing well!" > /var/www/html/index.html
